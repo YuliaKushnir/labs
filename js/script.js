346c61f44020a8eafdd86c1a849f248be909dbfd
LavY <148194874+YuliaKushnir@users.noreply.github.com>
2024-01-31 22:40:54 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let html = document.getElementById("button1").innerHTML;
+
+function changingContent1() {
+    document.getElementById("content").innerHTML = html;
+}
+
+let html2 = document.getElementById("button2").innerHTML;
+function changingContent2() {
+    document.getElementById("content").innerHTML = html2;
+}
+
+let html3 = document.getElementById("button3").innerHTML;
+function changingContent3() {
+    document.getElementById("content").innerHTML = html3;
+}
+
+let html4 = document.getElementById("button4").innerHTML;
+function changingContent4() {
+    document.getElementById("content").innerHTML = html4;
+}
+
+let html5 = document.getElementById("button5").innerHTML;
+function changingContent5() {
+    document.getElementById("content").innerHTML = html5;
+}
+
+let html6 = document.getElementById("button6").innerHTML;
+function changingContent6() {
+    document.getElementById("content").innerHTML = html6;
+}
+
+let html7 = document.getElementById("button7").innerHTML;
+function changingContent7() {
+    document.getElementById("content").innerHTML = html7;
+}
+
+let html8 = document.getElementById("button8").innerHTML;
+function changingContent8() {
+    document.getElementById("content").innerHTML = html8;
+}
+
